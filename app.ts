f61d9dbd5b862113409e7c17867b8b5460538b2e
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-20 01:29:46 +0000

    타입정의 및 추론 알아보기

--- app.ts
+++ app.ts
@@ -1,4 +1,18 @@
-function add(num1: number, num2: number) {
-  console.log(num1 + num2);
-}
-add(1, 2);
+// a의 타입을 추론해줌
+let a = "hello";
+a = "bye";
+a = 123; //타입에러
+
+// 타입을 직접 정의해줄수도 있다.
+let b: boolean = "x"; //타입에러
+
+//배열도
+let c: number[] = [1, 2, 3, 4];
+c.push("abc"); //타입에러
+
+//객체도
+let player = {
+  name: "soyoung",
+};
+player.name = 1; //타입에러
+player.hello(); //에러
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,6 +1,22 @@
-function add(num1, num2) {
-    console.log(num1 + num2);
-}
-add(1, 2);
+// a의 타입을 추론해줌
+var a = "hello";
+a = "bye";
+a = 123; //타입에러
+// 타입을 직접 정의해줄수도 있다.
+var b = "x"; //타입에러
+//배열도
+var c = [
+    1,
+    2,
+    3,
+    4
+];
+c.push("abc"); //타입에러
+//객체도
+var player = {
+    name: "soyoung"
+};
+player.name = 1; //타입에러
+player.hello(); //에러
 
 //# sourceMappingURL=index.7c0ccee6.js.map
