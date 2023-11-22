a5bf5bf8bf63ab011134d39b21ce277fdadc7147
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-22 00:55:15 +0000

    parameter의 갯수가 다른 오버로딩

--- app.ts
+++ app.ts
@@ -1,19 +1,11 @@
-type Config = {
-  path: string;
-  state: object;
+//매개변수(parameter) 갯수가 다른 오버로딩
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
 };
 
-//함수에서 사용할 오버로딩 타입정의 예시
-type Push = {
-  (path: string): void;
-  (config: Config): void;
-};
-
-//실제 함수에서 사용하는 법
-const push: Push = (config) => {
-  if (typeof config === "string") {
-    console.log(config);
-  } else {
-    console.log(config.path, config.state);
-  }
+//매개변수가 다른 함수의 타입을 오버로딩하려면 매개변수(optional)에 ?와 타입을 지정해주어야한다.
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
 };
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,7 +1,7 @@
-//실제 함수에서 사용하는 법
-var push = function(config) {
-    if (typeof config === "string") console.log(config);
-    else console.log(config.path, config.state);
+//매개변수가 다른 함수의 타입을 오버로딩하려면 매개변수(optional)에 ?와 타입을 지정해주어야한다.
+var add = function(a, b, c) {
+    if (c) return a + b + c;
+    return a + b;
 };
 
 //# sourceMappingURL=index.7c0ccee6.js.map
