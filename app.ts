c5ef198c5b50301b06e64e4ef5413b4765f670c7
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-23 02:00:23 +0000

    과제

--- app.ts
+++ app.ts
@@ -1,13 +1,78 @@
-//제네릭 사용법
-type SuperPrint = {
-  <T>(arr: T[]): void;
-};
+//call signatures
+type Last={
+  <T>(arr:T[]):void
+  }
 
-const superPrint: SuperPrint = (arr) => {
-  arr.forEach((element) => {
-    console.log(element);
-  });
-};
-const arr = [0, 1, 2, 3, 4];
-superPrint(arr);
-superPrint([true, false, true]);
+type Prepend=<T>(arr:T[],item:T)=>T[]
+
+type Mix=<T,V>(arr1:T[],arr2:V[])=>(T|V)[]
+
+type Count=<T>(arr:T[])=>number
+
+type FindIndex=<T>(arr:T[],item:T)=>number|null
+
+type Slice=<T>(arr:T[],startIndex:number,endIndex?:number)=>T[]
+
+//arr
+const arr1=[1,2,3,4,5,10]
+const arr2=["a","b","c"]
+
+
+//function
+const last:Last=(arr)=>arr[arr.length-1]
+
+const prepend:Prepend=(arr,item)=>{
+  const newArr=[];
+  newArr.push(item);
+  for(var i=0;i<arr.length;i++){
+      newArr.push(arr[i]);
+  }
+  
+  return newArr;
+  }
+
+const mix:Mix=(arr1,arr2)=>{
+  const newArr:any[]=[];
+
+  arr1.forEach((i)=>{
+      newArr.push(i);
+  })
+  arr2.forEach((i)=>{
+      newArr.push(i)
+  })
+
+  return newArr;
+}
+
+const count:Count=(arr)=>arr.length
+
+const findIndex:FindIndex=(arr,item)=>{
+  for(var i=0;i<arr.length;i++){
+      if(arr[i]===item){
+          return i;
+      }
+  }
+  return null;
+}
+
+const slice:Slice=(arr,startIndex,endIndex?)=>{
+  const newArr:any[]=[];
+  if(endIndex){
+      for(var i=startIndex;i<endIndex;i++){
+          newArr.push(arr[i]);
+      }
+  }else{
+      for(var i=startIndex;i<arr.length;i++){
+          newArr.push(arr[i]);
+      }
+  }
+  return newArr;
+}
+
+//test
+//console.log(last(arr1));
+//console.log(prepend(arr1,100));
+//console.log(mix(arr1,arr2));
+//console.log(count(arr1));
+//console.log(findIndex(arr1,7));
+//console.log(slice(arr1,3,5));
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,20 +1,57 @@
-var superPrint = function(arr) {
-    arr.forEach(function(element) {
-        console.log(element);
-    });
-};
-var arr = [
-    0,
+//arr
+var arr1 = [
     1,
     2,
     3,
-    4
+    4,
+    5,
+    10
 ];
-superPrint(arr);
-superPrint([
-    true,
-    false,
-    true
-]);
+var arr2 = [
+    "a",
+    "b",
+    "c"
+];
+//function
+var last = function(arr) {
+    return arr[arr.length - 1];
+};
+var prepend = function(arr, item) {
+    var newArr = [];
+    newArr.push(item);
+    for(var i = 0; i < arr.length; i++)newArr.push(arr[i]);
+    return newArr;
+};
+var mix = function(arr1, arr2) {
+    var newArr = [];
+    arr1.forEach(function(i) {
+        newArr.push(i);
+    });
+    arr2.forEach(function(i) {
+        newArr.push(i);
+    });
+    return newArr;
+};
+var count = function(arr) {
+    return arr.length;
+};
+var findIndex = function(arr, item) {
+    for(var i = 0; i < arr.length; i++){
+        if (arr[i] === item) return i;
+    }
+    return null;
+};
+var slice = function(arr, startIndex, endIndex) {
+    var newArr = [];
+    if (endIndex) for(var i = startIndex; i < endIndex; i++)newArr.push(arr[i]);
+    else for(var i = startIndex; i < arr.length; i++)newArr.push(arr[i]);
+    return newArr;
+}; //test
+ //console.log(last(arr1));
+ //console.log(prepend(arr1,100));
+ //console.log(mix(arr1,arr2));
+ //console.log(count(arr1));
+ //console.log(findIndex(arr1,7));
+ //console.log(slice(arr1,3,5));
 
 //# sourceMappingURL=index.7c0ccee6.js.map
