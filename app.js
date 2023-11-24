9134704d8e2992867376d26dafb0be6c027409ba
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-24 04:35:34 +0000

    assignment2

--- app.js
+++ app.js
@@ -1,40 +1,112 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-//abstract class
-var User = /** @class */ (function () {
-    //따로 class에 parameter를 선언할 필요 없이 constructor에서 한번에 parameter 선언 및 초기화
-    function User(firstName, lastName, nickname) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.nickname = nickname;
+var Word = /** @class */ (function () {
+    function Word(term, def) {
+        this.term = term;
+        this.def = def;
     }
-    User.prototype.getFullName = function () {
-        return "".concat(this.firstName, " ").concat(this.lastName);
-    };
-    return User;
+    return Word;
 }());
-var Player = /** @class */ (function (_super) {
-    __extends(Player, _super);
-    function Player() {
-        return _super !== null && _super.apply(this, arguments) || this;
+var Dict = /** @class */ (function () {
+    function Dict() {
+        this.wordList = {};
     }
-    Player.prototype.getNickname = function () {
-        return this.nickname;
+    Dict.prototype.add = function (word) {
+        if (!this.wordList[word.term]) {
+            this.wordList[word.term] = word.def;
+            console.log("".concat(word.term, " added"));
+        }
+        else {
+            console.log("already exists");
+        }
     };
-    return Player;
-}(User));
-var soyoung = new Player("seo", "soyoung", "soyoungee");
-console.log(soyoung.getFullName);
+    Dict.prototype.get = function (term) {
+        return this.wordList[term];
+    };
+    Dict.prototype.delete = function (word) {
+        if (this.wordList[word.term]) {
+            delete this.wordList[word.term];
+            console.log("".concat(word.term, " word deleted"));
+        }
+        else {
+            console.log("not exists");
+        }
+    };
+    Dict.prototype.update = function (word) {
+        if (this.wordList[word.term]) {
+            this.wordList[word.term] = word.def;
+            console.log("1 word updated");
+        }
+        else {
+            console.log("not exists");
+        }
+    };
+    Dict.prototype.showAll = function () {
+        var _this = this;
+        console.log("<dictionary showAll> total= ".concat(Object.keys(this.wordList).length));
+        Object.keys(this.wordList).forEach(function (i) {
+            console.log("".concat(i, ": ").concat(_this.wordList[i]));
+        });
+    };
+    Dict.prototype.count = function () {
+        return Object.keys(this.wordList).length;
+    };
+    Dict.prototype.upsert = function (word) {
+        this.wordList[word.term] = word.def;
+    };
+    Dict.prototype.exists = function (word) {
+        if (this.wordList[word.term]) {
+            console.log("this is here");
+        }
+        else {
+            console.log("not exists");
+        }
+    };
+    Dict.prototype.bulkAdd = function (words) {
+        var _this = this;
+        var i = 0;
+        words.forEach(function (word) {
+            if (!_this.wordList[word.term]) {
+                _this.wordList[word.term] = word.def;
+                i++;
+            }
+            else {
+                console.log("".concat(word.term, " is already exists"));
+            }
+        });
+        console.log("Total of ".concat(i, " word added"));
+    };
+    Dict.prototype.bulkDelete = function (words) {
+        var _this = this;
+        var i = 0;
+        words.forEach(function (word) {
+            if (_this.wordList[word.term]) {
+                delete _this.wordList[word.term];
+                i++;
+            }
+            else {
+                console.log("".concat(word.term, " is not exists"));
+            }
+        });
+        console.log("Total of ".concat(i, " word deleted"));
+    };
+    return Dict;
+}());
+console.log("---------------start");
+//new parameter
+var dict = new Dict();
+var kimchWord = new Word("kimch", "김치");
+var eggWord = new Word("egg", "달걀");
+//test
+//dict.add(kimchWord);
+//dict.add(eggWord);
+// console.log(dict);
+// console.log(dict.get("kimch"));
+// dict.delete(kimchWord);
+// console.log(dict);
+//dict.showAll();
+//console.log(dict.count());
+// dict.bulkAdd([kimchWord,eggWord]);
+// dict.showAll();
+// dict.delete(kimchWord);
+// dict.bulkDelete([kimchWord,eggWord]);
+// dict.showAll();
+console.log("---------------closed");
--- app.ts
+++ app.ts
@@ -1,23 +1,123 @@
-//abstract class
-abstract class User {
-  //따로 class에 parameter를 선언할 필요 없이 constructor에서 한번에 parameter 선언 및 초기화
-  constructor(
-    protected firstName: string,
-    protected lastName: string,
-    public nickname: string,
-  ) {}
-  //abstract method
-  abstract getNickname(): string;
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
+type WordList={
+  [key:string]:string
+}
+
+class Word{
+  public term:string
+  public def:string
+  constructor(term:string,def:string){
+      this.term=term
+      this.def=def
   }
 }
 
-class Player extends User {
-  getNickname(): string {
-    return this.nickname;
+class Dict{
+  private wordList:WordList
+  constructor(){this.wordList={}}
+
+  add(word:Word):void{
+      if(!this.wordList[word.term]){
+          this.wordList[word.term]=word.def;
+          console.log(`${word.term} added`)
+      }else{
+          console.log("already exists")
+      }
+  }
+  get(term:string):string{
+      return this.wordList[term];
+  }
+  delete(word:Word):void{
+      if(this.wordList[word.term]){
+          delete this.wordList[word.term]
+          console.log(`${word.term} word deleted`)
+
+      }else{
+          console.log("not exists")
+      }
+  }
+  update(word:Word):void{
+      if(this.wordList[word.term]){
+          this.wordList[word.term]=word.def;
+          console.log("1 word updated")
+
+      }else{
+          console.log("not exists")
+      }
+  }
+  showAll():void{
+      console.log(`<dictionary showAll> total= ${Object.keys(this.wordList).length}`)
+      Object.keys(this.wordList).forEach((i)=>{
+          console.log(`${i}: ${this.wordList[i]}`);
+      })
+  }
+  count():number{
+      return Object.keys(this.wordList).length;
+  }
+  upsert(word:Word):void{
+      this.wordList[word.term]=word.def;
+  }
+  exists(word:Word){
+      if(this.wordList[word.term]){
+          console.log("this is here");
+      }else{
+          console.log("not exists")
+      }
+  }
+  bulkAdd(words:Word[]):void{
+      var i=0;
+      words.forEach((word)=>{
+          if(!this.wordList[word.term]){
+              this.wordList[word.term]=word.def;
+              i++
+          }else{
+              console.log(`${word.term} is already exists`)
+          }
+      })
+      console.log(`Total of ${i} word added`);
+  }
+  bulkDelete(words:Word[]):void{
+      var i=0;
+      words.forEach((word)=>{
+          if(this.wordList[word.term]){
+              delete this.wordList[word.term]
+              i++
+          }else{
+              console.log(`${word.term} is not exists`)
+          }
+      })
+      console.log(`Total of ${i} word deleted`);
+  }
   }
 }
 
-const soyoung = new Player("seo", "soyoung", "soyoungee");
-console.log(soyoung.getFullName);
+console.log("---------------start")
+
+//new parameter
+const dict=new Dict();
+const kimchWord=new Word("kimch","김치");
+const eggWord=new Word("egg","달걀")
+
+
+//test
+
+//dict.add(kimchWord);
+//dict.add(eggWord);
+// console.log(dict);
+
+// console.log(dict.get("kimch"));
+
+// dict.delete(kimchWord);
+// console.log(dict);
+
+//dict.showAll();
+
+//console.log(dict.count());
+
+// dict.bulkAdd([kimchWord,eggWord]);
+// dict.showAll();
+
+// dict.delete(kimchWord);
+// dict.bulkDelete([kimchWord,eggWord]);
+
+// dict.showAll();
+console.log("---------------closed")
